83b27a9fbabfd6072f5f1893244b28013b321fad
stealthycoin <john.w.carlyle@gmail.com>
2013-05-12 01:38:11 -0700

    Added sound example

--- map.js
+++ map.js
@@ -1,10 +1,11 @@
 function getN() { return 2; }
-function getSeq() { return "BCBBBAABBCBCCAAABBACAAACCABBABCCACBCBCAC"; }
+function getSeq() { return "DDDBDDAABDDDAADAADDD"; }
 
 function mapping(key) {
     map = {};
+    map['D'] = "<object height=\"100\" width=\"100\" data=\"./resources/horse.wav\"></object>";
+    map['A'] = "<img src=\"./resources/godzilla.jpg\" />";
     map['B'] = "<img src=\"./resources/george.jpg\">";
     map['C'] = "<img src=\"./resources/al.jpg\">";
-    map['A'] = "<img src=\"./resources/godzilla.jpg\" />";
     return map[key];
 }
