cd37b8148eeb522ca30ca694d281c3e2fcfe9c03
stealthycoin <john.w.carlyle@gmail.com>
2013-05-12 01:55:26 -0700

    Fixed examples

--- map.js
+++ map.js
@@ -1,11 +1,12 @@
+function getDelay() { return 2000; }
 function getN() { return 2; }
-function getSeq() { return "DDDBDDAABDDDAADAADDD"; }
+function getSeq() { return "CDADBDCCCB"; }
 
 function mapping(key) {
     map = {};
-    map['D'] = "<object height=\"100\" width=\"100\" data=\"./resources/horse.wav\"></object>";
     map['A'] = "<img src=\"./resources/godzilla.jpg\" />";
     map['B'] = "<img src=\"./resources/george.jpg\">";
     map['C'] = "<img src=\"./resources/al.jpg\">";
+    map['D'] = "<object height=\"0\" width=\"0\" data=\"./resources/horse.wav\"></object>";
     return map[key];
 }
